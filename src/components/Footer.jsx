0372f5c0ee355bf1211114bbf1be93b8b7428170
Anish <xevenbiswas@gmail.com>
2023-12-06 13:00:14 +0530

    works

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+      <div className="md:fixed bottom-2 max-w-[200px] mx-auto right-8 border py-1 px-3 rounded-md shadow border-zinc-600 cursor-default mb-4 md:mb-1 bg-neutral-800/40">
+        Made with ❤️ by{" "}
+        <a
+          href="https://bento.me/anish7"
+          target="__blank"
+          className="text-rose-600"
+        >
+          Anish
+        </a>
+      </div>
+    );
+  };
+  
+  export default Footer;
+  
