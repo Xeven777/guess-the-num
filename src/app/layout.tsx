08e45211001b4441c27d37ad3a302c5d5ee63009
Anish <xevenbiswas@gmail.com>
2023-12-15 18:49:55 +0530

    OG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Guess The Number 7️⃣",
   description: "A Simple Guessing Game made with Next.js",
+  openGraph: {
+    type: "website",
+    url: "https://guess-d-num.netlify.app/",
+    title: "Guess The Number 7️⃣",
+    description: "A Simple Guessing Game made with Next.js",
+    images: [
+      {
+        url: "https://i.imgur.com/ZqVmpv2l.png",
+        width: 1200,
+        height: 630,
+        alt: "Guess The Number 7️⃣",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
