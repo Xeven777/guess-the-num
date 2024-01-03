2fd7e7be2bd464916b4480293cda66db25cbf4fa
Anish <xevenbiswas@gmail.com>
2024-01-03 17:18:38 +0530

    added app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Guess The Number 7️⃣",
   description: "A Simple Guessing Game made with Next.js",
+  metadataBase: new URL("https://guess-d-num.netlify.app/"),
   openGraph: {
     type: "website",
     url: "https://guess-d-num.netlify.app/",
